a1ff0f39cd56da90cda51659923c3482f7172fb5
Shane Xu <xusheng0711@gmail.com>
2021-01-16 11:39:41 +0800

    mpv wrap

--- cmd/mpv/main.go
+++ cmd/mpv/main.go
@@ -7,11 +7,15 @@ import (
 )
 
 func main() {
-	title := os.Args[2]
-	msgLevel := os.Args[3]
-	msgLevel = strings.ReplaceAll(msgLevel, ":", ",")
-	restArgs := os.Args[4:]
-	args := []string{"--title=" + title, msgLevel}
-	args = append(args, restArgs...)
-	syscall.Exec("/usr/local/bin/mpv", args, nil)
+	if len(os.Args) >= 6 {
+		title := os.Args[2]
+		msgLevel := os.Args[3]
+		msgLevel = strings.ReplaceAll(msgLevel, ":", ",")
+		restArgs := os.Args[4:]
+		args := []string{"--vid=no", "--title=" + title, msgLevel}
+		args = append(args, restArgs...)
+		syscall.Exec("/usr/local/bin/mpv", args, nil)
+	} else {
+		syscall.Exec("/usr/local/bin/mpv", os.Args[1:], nil)
+	}
 }
